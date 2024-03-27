aa6da4332be220bc70f4a86a34076c6a0786d831
AnhQuan202 <120768861+anhquan1812@users.noreply.github.com>
2024-03-27 11:25:02 +0700

    remove update, delete; fix search function

--- datn/app/Http/Controllers/CustomerController.php
+++ datn/app/Http/Controllers/CustomerController.php
@@ -30,40 +30,13 @@ class CustomerController extends Controller
             return response()->json(['error' => $e->getMessage(), 'message' => 'Đã có lỗi xảy ra'], 500);
         }
     }
-    public function update($id, CustomerRequest $request)
-    {
-        try {
-            $customer = customer::update($request->all());
-            return response(['message' => 'Dữ liệu đã được thêm thành công', 'data' => $customer], 201);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage(), 'message' => 'Đã có lỗi xảy ra'], 500);
-        }
-    }
-    public function delete($id)
-    {
-        try {
-            $customer = customer::find($id);
-            if ($customer) {
-                $customer->delete();
-                return response(['message' => 'Đã xóa thành công', 'data' => $customer], 200);
-            } else {
-                return response(['error' => 'Không tìm thấy', 'data' => []], 200);
-            }
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage(), 'message' => 'Đã có lỗi xảy ra'], 500);
-        }
-    }
     public function search(Request $request)
     {
-        $request->validate([
-            'keyword' => 'required|string',
-        ]);
-
         $keyword = $request->input('keyword');
 
         $customers = customer::where('name', 'like', "%$keyword%")->get();
 
-        return response()->json(['customers' => $customers]);
+        return response()->json(['data' => $customers]);
     }
     
 }
